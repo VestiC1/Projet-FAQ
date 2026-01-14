59a2a264464c5c2e3fa889cd18e17af9ca2b9664
Jonathan Caillaux <jonathan.caillaux@protonmail.com>
2026-01-14 10:33:56 +0100

    FEAT dataloder

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -1,26 +1,16 @@
 import json
 from pathlib import Path
-from pprint import pprint
-
-ROOT = Path(__file__).parent.parent
-DATA = ROOT / "data"
-
-faq    = DATA / "faq-base-6964b97cf0c25947575840.json"
-golden = DATA / "golden-set-6964b9874cff1935078155.json"
+from config import GOLDEN_PATH, FAQ_PATH
 
 def load_json(path: Path):
+    if not path.exists():
+        raise FileNotFoundError(f"File not found: {path}")
+    
     with open(path, "r", encoding="utf-8") as f:
         return json.load(f)
 
 def load_faq():
-    return load_json(faq)
+    return load_json(FAQ_PATH)
 
 def load_golden():
-    return load_json(golden)
-
-def main():
-    faq_data = load_faq()
-    golden_data = load_golden()
-
-if __name__ == "__main__":
-    main()
+    return load_json(GOLDEN_PATH)
