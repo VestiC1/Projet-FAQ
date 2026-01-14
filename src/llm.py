e19b0962ae960ac4dc7334d9e38431036719d1ae
VestiC1 <steve.dos.santos.41@gmail.com>
2026-01-14 16:59:44 +0100

    FEAT strategy A implemented

--- src/llm.py
+++ src/llm.py
@@ -2,31 +2,30 @@ from huggingface_hub import InferenceClient
 from config import HF_TOKEN, LLMNAME
 
 
-class LLMChatCompletion:
+from huggingface_hub import InferenceClient
 
+class LLMChatCompletion:
     def __init__(self, system_prompt, max_tokens):
 
-        # HF client initialisation
-        self.client = InferenceClient(
-            model=LLMNAME,
-            token=HF_TOKEN,
-        )
+        self.client = InferenceClient(model=LLMNAME, token=HF_TOKEN)
 
         self.max_tokens = max_tokens
 
-        messages = [{
-            "role": "user",
-            "content": system_prompt,
-        }]
+        self.messages = [
+            {"role": "system", "content": system_prompt}
+        ]
 
-        result = self.client.chat_completion(messages=messages, max_tokens=max_tokens)
-        print(result)
-    
     def reply(self, prompt):
+
         return self.client.chat_completion(
-            messages=[{
-                "role": "user",
-                "content": prompt,
-            }], 
+            messages=self._build_message(prompt=prompt), 
             max_tokens=self.max_tokens
         )
+
+
+    def _build_message(self, prompt):
+
+        return self.messages + [{
+            "role": "user", 
+            "content": prompt
+        }]
