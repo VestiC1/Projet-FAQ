183da8ce8ab9ea2917b55325242acb100392d5f9
Jonathan Caillaux <jonathan.caillaux@protonmail.com>
2026-01-14 12:49:49 +0100

    FEAT embedding

--- scripts/compute_embeding.py
+++ scripts/compute_embeding.py
@@ -12,7 +12,8 @@ def data_extract(json_data):
     return pd.DataFrame(
         {
             "id" : [ doc['id'] for doc in json_data['faq']],
-            "text" : [ f"Question : {doc['answer']} Réponse : {doc['question']}" for doc in json_data['faq']],
+            "question" : [ doc['question'] for doc in json_data['faq']],
+            "answer" : [ doc['answer'] for doc in json_data['faq']],
         }
     )
 
@@ -21,7 +22,7 @@ def main():
     df = data_extract(faq_data)
     model_name = "sentence-transformers/all-MiniLM-L6-v2"
     model = SentenceTransformer(model_name)
-    df['embedding'] = df['text'].apply(lambda x : compute_embeddings(model, x))
+    df['embedding'] = df[['question', 'answer']].apply(lambda x : compute_embeddings(model, f"Question : {x['answer']} Réponse : {x['question']}"), axis=1)
     print(df.head())
 if __name__ == "__main__":
     main()
