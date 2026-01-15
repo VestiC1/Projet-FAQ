0d824bee9bdebd6c38eb11be51e0cd711349a49d
Jonathan Caillaux <jonathan.caillaux@protonmail.com>
2026-01-15 09:25:13 +0100

    CHORe implementation strategy A

--- src/strategies/strategya.py
+++ src/strategies/strategya.py
@@ -1,6 +1,7 @@
 from src.llm import LLMChatCompletion
-from config import HF_TOKEN, LLMNAME
-
+from config import HF_TOKEN, LLMNAME, DATA_DIR
+from scripts.data_loader import load_golden
+import json
 def main():
     system_prompt = """
     Tu es un assistant IA français dédié à répondre aux questions administratives pour la Communauté de Communes Val de Loire Numérique.
@@ -12,14 +13,28 @@ def main():
     - Format : simple court et informatif.
     - Pas d’hallucinations : si incertain, considère que tu ne connais pas.
     - Périmètre : état civil, urbanisme, déchets, transports, petite-enfance, social, vie associative, élections, logement, culture/sport, fiscalité, eau/assainissement.
-    - si c'est hors perimetre considère que tu ne connais pas
+    - Si c'est hors périmètre considère que tu ne connais pas
     """
 
     llm = LLMChatCompletion(system_prompt=system_prompt, max_tokens=200)
 
-    result = llm.reply(prompt="Quelles sont les démarches pour déclarer une naissance ?")
+    #result = llm.reply(prompt="Quelles sont les démarches pour déclarer une naissance ?")
     print()
-    print(result)
+    #print(result)
+
+    answers = []
+    golden_data = load_golden()['golden_set']
+    for item in golden_data:
+        q = item['question']
+        print("Q:", q)
+        result = llm.reply(prompt=q)
+        print("A:", result)
+        answers.append({
+            'question': q,
+            'answer': str(result)
+        })
+    with open(DATA_DIR / 'llm_answers.json', 'w', encoding='utf-8') as f:
+        json.dump(answers, f, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
     main()
